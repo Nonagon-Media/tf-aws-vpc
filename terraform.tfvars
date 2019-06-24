936a483dc13be5d82ae2af8930e74f31e838932e
Troy Thompson <troycthompson@gmail.com>
2019-06-24 09:06:09 -0700

    Moved to tfvars

---
+++ terraform.tfvars
@@ -0,0 +1,18 @@
+### GLOBALS ###
+aws_region = "us-west-2"
+default_ami = "ami_0db180c518750ee4f"
+
+### VPC ###
+n9n_cidr = "192.168.0.0/16"
+
+### PUBLIC SUBNETS ###
+operations_public = "192.168.1.0/24"
+applications_public = "192.168.2.0/24"
+development_public = "192.168.3.0/24"
+
+### PRIVATE SUBNETS ###
+operations_private = "192.168.10.0/24"
+applications_private = "192.168.20.0/24"
+development_private = "192.168.30.0/24"
+
+
--- variables.tf
+++ variables.tf
@@ -4,19 +4,19 @@
 
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-2"
+  #default = "us-west-2"
 }
 
 variable "default_ami" {
   description = "Ubuntu 18.04 SSD"
-  default = "ami-005bdb005fb00e791"
+  #default = "ami_0db180c518750ee4f"
 }
 
-variable "key_path" {
-	# In a real environme dont do this
-	description = "SSH public key"
-	default = "/home/sthompson/.ssh/satellite.pub"
-}
+# # variable "key_path" {
+# 	# In a real environme dont do this
+# 	description = "SSH public key"
+# 	default = "/home/sthompson/.ssh/satellite.pub"
+# }
 
 
 ###########
@@ -25,7 +25,7 @@ variable "key_path" {
 
 variable "n9n_cidr" {
 	description = "CIDR for the N9N VPC"
-	default = "192.168.0.0/16"
+	#default = "192.168.0.0/16"
 }
 
 ###############
@@ -34,30 +34,30 @@ variable "n9n_cidr" {
 
 variable "operations_public" {
 	description = "CIDR for public subnet in AZ a _ Operations"
-	default = "192.168.1.0/24"
+	#default = "192.168.1.0/24"
 }
 
 variable "applications_public" {
 	description = "CIDR for public subnet in AZ b _ Applications"
-	default = "192.168.2.0/24"
+	#default = "192.168.2.0/24"
 }
 
 variable "development_public" {
 	description = "CIDR for public subnet in AZ c _ Development"
-	default = "192.168.3.0/24"
+	#default = "192.168.3.0/24"
 }
 
 variable "operations_private" {
 	description = "CIDR for private subnet in AZ a _ Operations"
-	default = "192.168.10.0/24"
+	#default = "192.168.10.0/24"
 }
 
 variable "applications_private" {
 	description = "CIDR for private subnet in AZ b _ Applications"
-	default = "192.168.20.0/24"
+	#default = "192.168.20.0/24"
 }
 
 variable "development_private" {
 	description = "CIDR for private subnet in AZ c _ Development"
-	default = "192.168.30.0/24"
+	#default = "192.168.30.0/24"
 }
